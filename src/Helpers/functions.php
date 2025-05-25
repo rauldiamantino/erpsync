b4eb990e385ebf6880a59ab2e711f837afb08718
Raul Diamantino <rauldiamantino25@gmail.com>
2025-05-25 00:00:31 -0300

    Feat: Implements debug in dark mode

--- src/Helpers/functions.php
+++ src/Helpers/functions.php
@@ -1,14 +1,23 @@
-<?php
+<?php function pr($data, $type = false) { ?>
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+    <title>Test</title>
+  </head>
+  <body class="bg-black text-gray-200">
+    <pre>
 
-function pr($data, $type = false) {
-  echo '<pre>';
-
-  if ($type) {
-    var_dump(print_r($data));
-  }
-  else {
-    print_r($data);
-  }
-
-  echo '</pre>';
-}
+    <?php if ($type) {
+      var_dump(print_r($data));
+    }
+    else {
+      print_r($data);
+    }
+    ?>
+  </body>
+  </html>
+  </pre>
+<?php } ?>
